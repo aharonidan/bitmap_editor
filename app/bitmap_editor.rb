351f3b8f2ca5b6676ed7f2ff5051f90fd90de4bf
Dan Aharoni <aharonidan@gmail.com>
2016-10-21 16:43:00 +0100

    make validations more readable

--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -5,14 +5,14 @@ class BitmapEditor
 
   COMMANDS =
     {
-      'I' => { number_of_args: 2, method: :create_image, image_required: false },
-      'C' => { number_of_args: 0, method: :clear_table, image_required: true  },
-      'L' => { number_of_args: 3, method: :colour_pixel, image_required: true  },
-      'H' => { number_of_args: 4, method: :colour_horizontal, image_required: true  },
-      'V' => { number_of_args: 4, method: :colour_vertical, image_required: true  },
-      'S' => { number_of_args: 0, method: :print, image_required: true  },
-      'X' => { number_of_args: 0, method: :exit_console, image_required: false  },
-      '?' => { number_of_args: 0, method: :show_help, image_required: false  },
+      'I' => { num_of_args: 2, method: :create_image,},
+      'C' => { num_of_args: 0, method: :clear_table, image_required: true  },
+      'L' => { num_of_args: 3, method: :colour_pixel, image_required: true, colour: true },
+      'H' => { num_of_args: 4, method: :colour_horizontal, image_required: true, colour: true   },
+      'V' => { num_of_args: 4, method: :colour_vertical, image_required: true, colour: true   },
+      'S' => { num_of_args: 0, method: :print, image_required: true  },
+      'X' => { num_of_args: 0, method: :exit_console},
+      '?' => { num_of_args: 0, method: :show_help},
     }
 
   SIZE_LIMIT = 250
@@ -41,24 +41,47 @@ class BitmapEditor
       end
     end
 
-
     def validate_input(input)
       result = nil
       option, *args = input.split
       command = COMMANDS[option]
 
-      if command == nil
+      if command_not_found?(command)
         puts 'unrecognised command :('
-      elsif command[:number_of_args] != args.length
+      elsif wrong_number_arguments?(command, args)
         puts 'wrong number of arguments :('
-      elsif command[:image_required] && image == nil
+      elsif image_is_missing?(command)
         puts 'image required for this operation :('
-      elsif command[:method] == :create_image && wrong_dimensions(*args)
+      elsif invalid_dimensions?(command, args)
+        puts 'image dimensions error, dimensions must be between 1 and 250 :('
+      elsif invalid_colour?(command, args)
+        puts 'colour error, please specify colour by a capital letter'
       else
         result = command
       end
 
       return result
+    end
+
+    def invalid_dimensions?(command, args)
+      command[:method] == :create_image && wrong_dimensions(*args)
+    end
+
+    def image_is_missing?(command)
+      image == nil && command[:image_required]
+    end
+
+    def command_not_found?(command)
+      command == nil
+    end
+
+    def wrong_number_arguments?(command, args)
+      command[:num_of_args] != args.length
+    end
+
+    def invalid_colour?(command, args)
+      colour = args[-1]
+      command[:colour] && !colour.match(/[A-Z]/)
     end
 
     def wrong_dimensions(rows, columns)
@@ -78,7 +101,7 @@ class BitmapEditor
       @image = Bitmap.new(m.to_i, n.to_i)
     end
 
-    def show_help(*args)
+    def show_help
       puts "? - Help
 I M N - Create a new M x N image with all pixels coloured white (O).
 C - Clears the table, setting all pixels to white (O).
