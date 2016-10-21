ad1991cc604224e85fbdc0ba302e1b2cb071afe5
Dan Aharoni <aharonidan@gmail.com>
2016-10-21 13:42:54 +0100

    no need to specify unused variables

--- app/bitmap.rb
+++ app/bitmap.rb
@@ -8,15 +8,15 @@ class Bitmap
     @table = create_table
   end
 
-  def create_table(*args)
+  def create_table
     Array.new(rows) { Array.new(columns, 'O') }
   end
 
-  def clear_table(*args)
+  def clear_table
     @table = create_table
   end
 
-  def print(*args)
+  def print
     result = ''
     table.each do |row|
       row.each do |cell|
